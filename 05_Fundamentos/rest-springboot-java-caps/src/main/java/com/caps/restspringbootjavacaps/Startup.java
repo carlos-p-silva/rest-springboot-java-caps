cae7fb8d052ced324e4d567b3f6a094b1cc1bdaa
Carlos Pereira <carlos.programacao@gmail.com>
2024-03-20 12:08:10 -0300

    Primeiro commit

---
+++ 05_Fundamentos/rest-springboot-java-caps/src/main/java/com/caps/restspringbootjavacaps/Startup.java
@@ -0,0 +1,14 @@
+package com.caps.restspringbootjavacaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
